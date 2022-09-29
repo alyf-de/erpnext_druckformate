775ac467ce02770ab487da3ff983ff2d2eff125c
barredterra <14891507+barredterra@users.noreply.github.com>
2022-09-29 22:41:17 +0530

    refactor: update.py

--- update.py
+++ update.py
@@ -18,14 +18,37 @@ PRINT_FORMATS = {
 	"purchase_order.jinja": "Bestellung",
 	"request_for_quotation.jinja": "Angebotsanfrage",
 }
+LETTER_HEAD = ("letter_head.jinja", "ALYF GmbH")
 
 
-def update_css():
-	input_path = f"{PRINT_STYLE_PATH}.scss"
-	output_path = f"{PRINT_STYLE_PATH}.css"
-	print(f"Building CSS {input_path} -> {output_path}")
-	run(["sass", "--style=compressed", input_path, output_path], check=True)
-	return output_path
+def get_updated_css(input_path: str, output_path: str) -> str:
+	typer.echo(f"Building CSS {input_path} -> {output_path}")
+	run(["sass", "--style=compressed", f"{PRINT_STYLE_PATH}.scss", output_path], check=True)
+	return pathlib.Path(output_path).read_text()
+
+
+def update_print_format(client: FrappeClient, source_path, css: str, target_name: str):
+	typer.echo(f"Syncing {source_path} -> Print Format '{target_name}'")
+	client.update(
+		{
+			"doctype": "Print Format",
+			"name": target_name,
+			"html": pathlib.Path(source_path).read_text(),
+			"css": css,
+		}
+	)
+
+
+def update_letter_head(client: FrappeClient, source_path: str, target_name: str):
+	typer.echo(f"Syncing {source_path} -> Letter Head '{target_name}'")
+
+	return client.update(
+		{
+			"doctype": "Letter Head",
+			"name": target_name,
+			"footer": pathlib.Path(source_path).read_text(),
+		}
+	)
 
 
 def main(username: str = None, password: str = None):
@@ -35,28 +58,17 @@ def main(username: str = None, password: str = None):
 	while not password:
 		password = getpass()
 
-	css = None
-	css_path = update_css()
-	css = pathlib.Path(css_path).read_text()
+	css = get_updated_css(f"{PRINT_STYLE_PATH}.scss", f"{PRINT_STYLE_PATH}.css")
 	client = FrappeClient(url=ERPNEXT_URL, username=username, password=password)
 	with os.scandir("print_format") as it:
 		for entry in it:
-			html = None
-
 			if entry.name not in PRINT_FORMATS:
 				continue
 
-			html = pathlib.Path(entry.path).read_text()
 			print_format_name = PRINT_FORMATS.get(entry.name)
-			print(f"Syncing {entry.name} -> Print Format '{print_format_name}'")
-			client.update(
-				{
-					"doctype": "Print Format",
-					"name": print_format_name,
-					"html": html,
-					"css": css,
-				}
-			)
+			update_print_format(client, entry.path, css, print_format_name)
+
+	update_letter_head(client, *LETTER_HEAD)
 
 
 if __name__ == "__main__":
